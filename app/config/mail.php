f1a2479dd908156a122d2324c8f1ea692210aab2
nasseralmanji <nhm919@hotmail.com>
2014-09-03 13:18:35 +0400

    MAILGUN

--- app/config/mail.php
+++ app/config/mail.php
@@ -10,13 +10,19 @@ return array(
 'username' => getenv('MANDRILL_USER'),
 'password' => getenv('MANDRILL_PASSWORD'),*/
 
-'driver' => 'smtp',
+/*'driver' => 'smtp',
 'host' => 'smtp.gmail.com',
 'port' => 465,
 'from' => array('address' => 'no-reply@ahleeen.com', 'name' => 'Ahleeen Team'),
 'encryption' => 'ssl',
 'username' => getenv('GMAIL_USER'),
-'password' => getenv('GMAIL_PASSWORD'),
+'password' => getenv('GMAIL_PASSWORD'),*/
 
+'driver' =>'smtp',
+'host' => 'smtp.mailgun.org',
+'port' => 587,
+'from' => array('address' => 'no-reply@ahleeen.com', 'name' => 'Ahleeen Team'),
+'username' => getenv('MAILGUN_USER'),
+'password' => getenv('MAILGUN_PASSWORD'),
 
 );
